22d23603eee998bc0baa7278b598d2dfdfaaf66a
Vishal Jagtap <89152210+vishalj1412@users.noreply.github.com>
2021-12-06 08:00:09 +0530

    Added: functional program

---
+++ Distance.py
@@ -0,0 +1,8 @@
+import math
+#calculate the distance using eucliden formula
+x=int(input("enter x co-ordinate : "))
+y=int(input("enter y co-ordinate : "))
+distance=math.sqrt(x*x+y*y)
+print(distance)
+ 
+ 
