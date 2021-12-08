d14548ac06dfdd0e1eec738fff6202b257f57462
Vishal Jagtap <89152210+vishalj1412@users.noreply.github.com>
2021-12-08 07:23:17 +0530

    Delete Distance.py

--- Distance.py
+++
@@ -1,8 +0,0 @@
-import math
-#calculate the distance using eucliden formula
-x=int(input("enter x co-ordinate : "))
-y=int(input("enter y co-ordinate : "))
-distance=math.sqrt(x*x+y*y)
-print(distance)
- 
- 
